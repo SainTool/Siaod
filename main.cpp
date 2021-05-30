33338b62ae165114f38a58e4f37f900f4721d82c
SainTool <78474049+SainTool@users.noreply.github.com>
2021-05-30 19:55:35 +0700

    lab 11

--- main.cpp
+++ main.cpp
@@ -1,133 +1,149 @@
 #include <iostream>
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
 using namespace std;
-//
-class List
-{
-public:
-	List();
-	~List();
-	void Push_up(int data);
-	void Pop_down();
-	void Cleaning();
-	int GetSize() { return Size; }
 
-	int& operator[](const int index);
-private:
+    void generate_rand(int n);
+    void generate_inc(int n);
+    void generate_dec(int n);
+    void print();
+    int control_sum();
+    int run_number();
+    void clear();
+    struct Data {
+        int num;
+        Data* next = NULL;
+    };
+    Data* head;
+    void print_node(Data* tmp);
 
-	class Node {
-	public:
-		Node* pNext;
-		int data;
-		Node(int data, Node* pNext=nullptr)
-		{
-			this->data = data;
-			this->pNext = pNext;
-		}
-	};
-	int Size;
-	Node *head;
-};
+void generate_rand(int n) {
+    if (n <= 0)
+        return;
+    if (head)
+        clear();
+    head = new Data;
+    Data* tmp2 = head;
+    int i = 0;
+    while (i < n) {
+        Data* tmp = new Data;
+        tmp->num = rand() % 100;
+        Data* tmp2 = head->next;
+        head->next = tmp;
+        tmp->next = tmp2;
+        i++;
+    }
+}
 
-List::List()
-{
-	Size = 0;
-	head = nullptr;
+void generate_dec(int n) {
+    if (n <= 0)
+        return;
+    if (head)
+        clear();
+    head = new Data;
+    Data* tmp2 = head;
+    int i = 0;
+    while (i < n) {
+        Data* tmp = new Data;
+        tmp->num = n - i;
+        Data* tmp2 = head->next;
+        head->next = tmp;
+        tmp->next = tmp2;
+        i++;
+    }
 }
-List::~List()
-{
-  Cleaning();
-}
-void List::Push_up(int data)
-{
-	if (head == nullptr) {
-		head = new Node(data);
-	}
-	else
-	{
-		Node* current = this->head;
-		while (current->pNext != nullptr) {
-			current = current->pNext;
-		}
-		current->pNext = new Node(data);
-	}
-	Size++;
-}
-void List::Pop_down()
-{
-	Node *temp = head;
-	head = head->pNext;
-	delete temp;
-	Size--;
-}
-void List::Cleaning()
-{
-	while (Size) {
-		Pop_down();
-	}
-}
-int& List::operator[](const int index)
-{
-	int counter = 0; // счётчик
-	Node* current = this->head;
-	while (current !=nullptr)
-	{
-		if (counter == index) {
-			return current->data;
-		}
-		current = current->pNext;
-		counter++;
-	}
 
+void generate_inc(int n) {
+    if (n <= 0)
+        return;
+    if (head)
+        clear();
+    head = new Data;
+    int i = 0;
+    while (i < n) {
+        Data* tmp = new Data;
+        tmp->num = i + 1;
+        Data* tmp2 = head->next;
+        head->next = tmp;
+        tmp->next = tmp2;
+        i++;
+    }
 }
-//
+
+void print_node(Data* tmp) { cout << tmp->num << " "; }
+
+void print() {
+    if (head == NULL)
+        return;
+    if (head->next != NULL) {
+        Data* tmp = head->next;
+        while (tmp != NULL) {
+            print_node(tmp);
+            tmp = tmp->next;
+        }
+        cout << endl;
+    }
+}
+
+int control_sum() {
+    if (head == NULL)
+        return 0;
+    int sum = 0;
+    if (head->next != NULL) {
+        Data* tmp = head->next;
+        while (tmp != NULL) {
+            sum += tmp->num;
+            tmp = tmp->next;
+        }
+    }
+    return sum;
+}
+
+int run_number() {
+    if (head == NULL)
+        return 0;
+    int k = 1;
+    if (head->next != NULL) {
+        Data* tmp = head->next;
+        while (tmp->next != NULL) {
+            if (tmp->num >= tmp->next->num)
+                k++;
+            tmp = tmp->next;
+        }
+    }
+    return k;
+}
+
+void clear() {
+    Data* tmp = head;
+    Data* next;
+    while (tmp != NULL) {
+        next = tmp->next;
+        delete tmp;
+        tmp = next;
+    }
+    head = NULL;
+}
 
 int main() {
-  srand(time(NULL));
-	List lst;
-	int sum=0;
-  int x;
-	for (int i = 0; i < 10; i++){
-		lst.Push_up(i+1);//добавление элемента
-	}
-  for (int i = 0; i < lst.GetSize(); i++)
-	{
-		cout << lst[i]<<" ";
-    sum+=lst[i];
-	}
-  cout<<endl<<sum<<" - kontrol sum"<<endl;
-  sum=0;
-  lst.Cleaning();
-  for (int i = 10; i>0;i--){
-      lst.Push_up(i);
-  }
-  for (int i = 0; i < lst.GetSize(); i++)
-	{
-		cout << lst[i]<<" ";
-    sum+=lst[i];
-	}
-  cout<<endl<<sum<<" - kontrol sum"<<endl;
-  sum=0;
-  lst.Cleaning();
-  for (int i = 0; i<lst.GetSize();i++){
-    x = rand() %10+1 ;
-    cout<<x;
-    lst.Push_up(x);
-    sum+=lst[i];
-  }
-  cout<<endl;
-  for (int i = 0; i < lst.GetSize(); i++)
-	{
-		cout << lst[i]<<" ";
-	}
-  cout<<endl<<sum<<" - kontrol sum"<<endl;
-  sum=0;
-  lst.Cleaning();
-	cout << lst.GetSize() << endl;//вывод размера
-	cout << endl;
-	lst.Cleaning();
-	cout << lst.GetSize();
-	return 0;
+    int n = 10;
+
+    srand(time(NULL));
+
+   generate_rand(n);
+   print();
+    cout << "Control Sum = " <<control_sum() << endl;
+    cout << "Run number = " << run_number() << endl << endl;
+
+    generate_dec(n);
+    print();
+    cout << "Control Sum = " << control_sum() << endl;
+    cout << "Run number = " << run_number() << endl << endl;
+
+    generate_inc(n);
+    print();
+    cout << "Control Sum = " << control_sum() << endl;
+    cout << "Run number = " << run_number() << endl << endl;
+    clear();
+
 }
+
+
